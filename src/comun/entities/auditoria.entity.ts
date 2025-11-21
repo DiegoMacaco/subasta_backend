58a13f873f3a3911e0557c467897d3734e357c80
DiegoMacaco <diego.guisbert.62103@usalesiana.edu.bo>
2025-11-21 00:36:35 -0400

    feat;adicionando entidad de auditoria

---
+++ src/comun/entities/auditoria.entity.ts
@@ -0,0 +1,23 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+export abstract class Auditoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  fechaCreacion!: Date;
+
+  @UpdateDateColumn()
+  fechaModificacion!: Date;
+
+  @Column({ default: true })
+  activo!: boolean;
+}
+
+
+const USUARIO_SISTEMA = 0
